9116a106c8adb7f41ac241dc3055af82d698a162
Lea97 <lea.brock1@web.de>
2018-05-17 19:50:06 +0200

    Change of the while Loop to an iterate over a collection

--- src/main/java/Program/Customer.java
+++ src/main/java/Program/Customer.java
@@ -22,25 +22,25 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		
-		Enumeration enum_rentals = rentals.elements();
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-		while (enum_rentals.hasMoreElements()) {
+		for (Enumeration enum_rentals = rentals.elements(); enum_rentals.hasMoreElements();) {
+			Rental rental = (Rental) enum_rentals.nextElement();
 			double thisAmount = 0;
-			Rental each = (Rental) enum_rentals.nextElement();
-			// determine amounts for each line
-			thisAmount = amountFor(each);
+			
+			thisAmount = amountFor(rental);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
+			result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
+		
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
